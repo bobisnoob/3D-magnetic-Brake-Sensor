662104c1f3f9a8bb92e6c2a4e5a1cd3e39612c51
Prabhjot singh <psingh0365@yahoo.in>
2017-12-04 15:42:25 -0500

    Feather_uart_tx

---
+++ lora_txx.ino
@@ -0,0 +1,70 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+/* for feather m0 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup()
+{
+  pinMode(RFM95_RST, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+rf95.setNodeAddress(11);
+Serial.println("LoRa radio init OK!");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.print("Set Freq to: ");
+    Serial.println(RF95_FREQ);
+    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+    // The default transmitter power is 13dBm, using PA_BOOST.
+    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+    // you can set transmitter powers from 5 to 23 dBm:
+    rf95.setTxPower(23, false);
+  }
+  digitalWrite(13, HIGH);
+}
+ 
+void loop()
+{
+  
+  int c;
+  int *a=&c;
+  if(Serial.available()) 
+  { 
+    //scan variables from serial terminal
+     c = Serial.read();
+     Serial.println("Sending:"); 
+     Serial.print(c);
+  }
+  delay(10);
+
+  //send data over the air
+  rf95.send((uint8_t *)a, 20);
+  delay(1000);
+}
